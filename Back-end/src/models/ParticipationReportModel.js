d1623e480d3761023264394c3bf40f85c6a5a849
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-12-29 10:48:06 +0700

    feat: change grade attributes

--- Back-end/src/models/ParticipationReportModel.js
+++ Back-end/src/models/ParticipationReportModel.js
@@ -6,12 +6,34 @@ const ParticipationReportSchema = new Schema({
   sectionId: { type: String, required: true },
   schoolYear: { type: String, required: true },
   semester: { type: Number, required: true },
-  grade10: { type: Number, default: 0 },
+  gradeMidterm: { 
+    type: Number, 
+    default: 0,
+    min: 0,
+    max: 10 
+  },
+  gradeFinal: { 
+    type: Number, 
+    default: 0,
+    min: 0,
+    max: 10 
+  },
+  gradeOthers: { 
+    type: Number, 
+    default: 0,
+    min: 0,
+    max: 10 
+  }
 });
 
 ParticipationReportSchema.index(
   { studentId: 1, sectionId: 1, schoolYear: 1, semester: 1 }, 
   { unique: true }
 );
+
+ParticipationReportSchema.virtual('gradeAverage').get(function() {
+  const average = (this.gradeMidterm + this.gradeFinal + this.gradeOthers) / 3;
+  return Number(average.toFixed(2));
+});
 
 module.exports = mongoose.model('ParticipationReport', ParticipationReportSchema);
